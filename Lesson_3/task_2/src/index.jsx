69fac40bd52b126002954e1f59fd95af9de81a3e
shema1 <shymechko.m.a@gmail.com>
2020-01-13 13:56:37 +0200

    commit

---
+++ Lesson_3/task_2/src/index.jsx
@@ -0,0 +1,24 @@
+import React,{Component} from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import "./search.scss";
+
+const rootElement = document.querySelector("#root");
+
+
+
+class SearchField extends Component{
+  render(){
+    return (
+      <div className="search">
+        <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
+        <div className="search__field">
+          <input type="text" className="search__input" />
+          <button className="search__button">Search</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<SearchField name="Tom" />, rootElement);
