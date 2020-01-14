e5816832a917958a3f9a71d6228d5d355681e172
shema1 <shymechko.m.a@gmail.com>
2020-01-14 10:17:24 +0200

    commit

--- Lesson_2/task_1/webpack.config.js
+++ Lesson_2/task_1/webpack.config.js
@@ -5,6 +5,7 @@ const webpack = require("webpack");
 module.exports = {
     entry: "./src/index.js",
     output: {
+        path: __dirname  + '/review_build',
         filename: "bundle.js"
     },
     module: {
