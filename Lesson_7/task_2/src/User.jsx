95e77fcc906770db9f0d512fbde3d2620dafe95b
shema1 <shymechko.m.a@gmail.com>
2020-01-21 14:24:55 +0200

    commit

--- Lesson_7/task_2/src/User.jsx
+++ Lesson_7/task_2/src/User.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const User = ({ name, age }) => {
   return (
-    <li key={age} className="user">
+    <li  className="user">
       <span className="user__name">{name}</span>
       <span className="user__age">{age}</span>
     </li>
