57c3eae0433fb770d183b6f1bf3416eeca7b30e8
shema1 <shymechko.m.a@gmail.com>
2020-01-24 11:54:07 +0200

    commit

--- Lesson_9/hw_1/src/UserList.jsx
+++ Lesson_9/hw_1/src/UserList.jsx
@@ -11,27 +11,22 @@ class UserList extends Component {
     this.setState({
       inputValue: event.target.value
     });
- 
   };
   render() {
-    const filterUsers = this.props.users
-      .filter(user => user.name == this.state. inputValue)
-      .map(user => <User key={user.id} name={user.name} age={user.age} />);
-
-    const defaultUsers = this.props.users.map(user => (
-      <User key={user.id} {...user} />
-    ));
-
+ 
+    let filterUsers = this.props.users
+      .filter(({ name }) =>
+        !this.state.inputValue ? true : name === this.state.inputValue
+      )
+      .map(({ id, name, age }) => <User key={id} name={name} age={age} />);
     return (
       <div>
         <Filter
           onChange={this.handleChange}
           filterText={this.state.inputValue}
-          count={this.state.inputValue?filterUsers.length:defaultUsers.length}
+          count={filterUsers.length}
         />
-        <ul className="users">
-          {this.state.inputValue?filterUsers:defaultUsers }
-        </ul>
+        <ul className="users">{filterUsers}</ul>
       </div>
     );
   }
