ffdd2cb59a0d7784eafe660ee876d157186e4379
shema1 <shymechko.m.a@gmail.com>
2020-02-13 09:32:04 +0200

    commit

--- Lesson_16/task_2/src/Home.jsx
+++ Lesson_16/task_2/src/Home.jsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      home
-      {/* <Link to="/products">To products</Link> */}
+
+    <div className="page__content">
+      <h1>🏠</h1>
+      <Link to="/products">To products</Link>
     </div>
   );
 };
