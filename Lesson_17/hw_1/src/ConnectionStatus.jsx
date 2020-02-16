0e88fc896d2db505c45049b493a349e1ec7fb4bd
shema1 <shymechko.m.a@gmail.com>
2020-02-16 11:42:35 +0200

    commit

---
+++ Lesson_17/hw_1/src/ConnectionStatus.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+const ConnectionStatus = () => {
+    const [isOnline, setIsOnline] = useState(true);
+
+    useEffect(()=>{
+        const offlineStatus =()=>{
+            setIsOnline(false)
+        }
+        const onlineStatus= ()=>{
+            setIsOnline(true)
+        }
+        window.addEventListener("offline", offlineStatus);
+        window.addEventListener("online", onlineStatus)
+
+        return()=>{
+            window.removeEventListener("offline", offlineStatus)
+            window.removeEventListener("online", onlineStatus)
+        }
+    })
+    return (
+       <div>
+           {isOnline?"online":"offline"}
+       </div>
+    );
+};
+
+export default ConnectionStatus;
